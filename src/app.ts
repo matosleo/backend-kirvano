c7cb83ec113f855a36e5f9686dc91fa0c1698622
Leonardo S. Matos <leonardo.matos@unimednatal.com.br>
2024-04-30 16:19:13 -0300

    feat: verficação de token em todas as requisições

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import { router } from './routes'
 export const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use(router)
 
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,30 @@ import { Router } from 'express'
 
 const router = Router()
 
+// This middleware is executed for every request to the router
+router.use((req, res, next) => {
+  const {access_token} = req.query
+
+  if(req.url == '/') {
+    return next()
+  }
+
+  if(!access_token || access_token != process.env.PASS_TOKEN) {
+    return res.status(401).send("Access unauthorized. Verify if your access token is correct.")
+  }
+  next()
+})
+
+router.post("/teste1", (request, response) => {
+  response.send('teste rota 1');
+})
+
+router.get("/teste2", (request, response) => {
+  response.send('teste rota 2');
+})
+
 // Main Route
-router.use("/", (request, response) => {
+router.get("/", (request, response, next) => {
   const fullUrl = request.protocol + '://' + request.get('host') + request.originalUrl;
   response.send(`This is a main route ${fullUrl}`);
 })
